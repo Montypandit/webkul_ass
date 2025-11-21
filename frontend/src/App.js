b16589d49402479c8568299626b4b85c5638da0d
monty787 <monty.2426mca1965@kiet.edu>
2025-11-21 15:08:34 +0530

    update code of frontend  files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,7 @@ function App() {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           
           <Route path="/allposts" element={
             <PrivateRoute>
